aafe270e913785d6dfe4ab9f746db97a307b9f52
Anna Karutina <anna.karutina@khk.ee>
2021-11-18 09:52:21 +0200

    OOP test - OOJS3 - Class Inheritance

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 // object creation
-let square = new Shape("square", 4, 5)
+let square = new Square(5)
 console.log(square)
 let triangle = new Shape("triangle", 3, 3)
 console.log(triangle)
 // object method calling
 square.calcPerimeter()
+square.calcArea()
 triangle.calcPerimeter()
