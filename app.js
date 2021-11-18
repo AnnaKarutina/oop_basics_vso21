248910c9cecc2e0d75ec0375a81709895f9dc7d8
Anna Karutina <anna.karutina@khk.ee>
2021-11-18 09:09:46 +0200

    OOP test - OOJS1

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-const anna = new Person('Anna', 'Karutina')
-console.log(anna)
-anna.greeting()
-const urmas = new Person('Urmas', 'Voevodin')
-console.log(urmas)
-urmas.greeting()
+// Shape constructor
+function Shape(name, sides, sideLength) {
+    // atributes - fields
+    this.name = name;
+    this.sides = sides;
+    this.sideLength = sideLength;
+    // functions - methods
+    this.calcPerimeter = function(){
+        // console.log(this.sides * this.sideLength)
+        this.perimeter = this.sides * this.sideLength
+        console.log(this.perimeter)
+    }
+}
+
+// object creation
+let square = new Shape("square", 4, 5)
+console.log(square)
+let triangle = new Shape("triangle", 3, 3)
+console.log(triangle)
+// object method calling
+square.calcPerimeter()
+triangle.calcPerimeter()
