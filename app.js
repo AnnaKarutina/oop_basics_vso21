5537d450788ea3dc34d1cacac79ac7c5ddc61ea5
Anna Karutina <anna.karutina@khk.ee>
2021-11-18 09:24:23 +0200

    OOP test - OOJS2

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-// Shape constructor
-function Shape(name, sides, sideLength) {
-    // atributes - fields
-    this.name = name;
-    this.sides = sides;
-    this.sideLength = sideLength;
-    // functions - methods
-    this.calcPerimeter = function(){
-        // console.log(this.sides * this.sideLength)
-        this.perimeter = this.sides * this.sideLength
-        console.log(this.perimeter)
-    }
-}
-
 // object creation
 let square = new Shape("square", 4, 5)
 console.log(square)
